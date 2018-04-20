e734d2ad23beae3ee446a36c2860c87e4aef5ba3
AppAcademyAccount <valerie.k.sui@gmail.com>
2018-04-19 21:38:45 -0700

    Wrote Map.rb

---
+++ W1D5/map.rb
@@ -0,0 +1,32 @@
+# Remember, a map stores information in key-value pairs, where the keys are always unique. When implemented with arrays, a map might look something like this: my_map = [[k1, v1], [k2, v2], [k3, v2], ...].
+#
+# Our Map class should have the following instance methods: assign(key, value), lookup(key), remove(key), show. Note that the assign method can be used to either create a new key-value pair or update the value for a pre-existing key. It's up to you to check whether a key currently exists!
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, val)
+    if lookup(key).nil?
+      self.push([key,val])
+    else
+      idx = self.find_index {|x| x.first == key}
+      self[idx] = [key, val]
+    end
+  end
+
+  def lookup(key)
+    self.select {|pair| pair.first == key}.first
+  end
+
+  def remove(key)
+    self.delete_if {|pair| pair.first == key}
+  end
+
+  def show
+    @map
+  end
+
+end
