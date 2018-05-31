bd1b57e36a7e5221881276f77b4fea00a444c5c9
Valerie Sui <valerie.k.sui@gmail.com>
2018-05-31 08:38:21 -0700

    Complete React Routes Intro

---
+++ HW7D4/components/blue.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Indigo from './indigo';
+
+
+class Blue extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className="blue"></h2>
+          <Link to='/blue' >Blue only</Link>
+          <Link to='/blue/indigo' >Add indigo</Link>
+
+         <Route path="/blue/indigo" component={Blue} />
+      </div>
+    );
+  }
+};
+
+export default Blue;
